324885f3d6cd55b02c9c79fa8801976e9befa323
rohan <rairohan14303@gmail.com>
2025-10-29 19:28:15 +0545

    new commit

--- javascript_examples/task2.js
+++ javascript_examples/task2.js
@@ -1,43 +1,49 @@
-//Challenge 1 – Beginner (Template literals, let/const, arrow functions)
+// //Challenge 1 – Beginner (Template literals, let/const, arrow functions)
 // let name = "rohan"
 // const age =22
 // let introduce = (name,age)=>{
 //     console.log(`Hi my name is ${name} and i am ${age}yrs old`)
 // }
 // introduce("rohan",22)
-//Challenge 2 – Intermediate (Destructuring, spread/rest operators, array methods)
-// Step 1: Create the array
-// const students = [
-//   { name: "Alice", score: 85 },
-//   { name: "Bob", score: 92 },
-//   { name: "Charlie", score: 78 }
-// ];
 
-// // Step 2: Destructure first student
-// const [firstStudent, ...restStudents] = students;
-
-// // Step 3: Function to calculate average using rest parameter
-// function getAverageScore(...studentsArray) {
-//   // studentsArray will be an array of scores
-//   const total = studentsArray.reduce((acc, score) => acc + score, 0);
-//   return total / studentsArray.length;
+// //challenge 2 - Beginner 
+// const numbers = [5,10,15,20]
+// function Sumstudents (...nums){
+//     let total = nums.reduce((acc,n)=>acc+n,0)
+//     console.log(total)
 // }
+// let Multstudents = (...num) =>{
+//  let total1 = num.reduce((acc,n1)=>acc*n1,1)
+// console.log(total1)
+// }
+// Sumstudents(...numbers)
+// Multstudents(...numbers)
 
-// // Step 4: Extract scores using spread operator
-// const scores = students.map(student => student.score);
-// const averageScore = getAverageScore(...scores);
+//Challenge 3
+// const nums = [10, 25, 5, 60, 15];
+// function FindMaxx(...numbers){
+//     let max = numbers.reduce((acc,num)=>acc > num ? acc:num)
+//     console.log(max)
+// }
+// FindMaxx(...nums)
+ 
+//Challenge 4
+//  const fruits = ["apple","banana","pear","orange"]
+// function countchar(...items){
+//     let totalcount = items.reduce((acc,items)=>acc+items.length,0)
+//     console.log(totalcount)
+// }
+// countchar(...fruits)
 
-// // Step 5: Log results
-// console.log("First student:", firstStudent);
-// console.log("Average score:", averageScore);
-const numbers = [5,10,15,20]
-function Sumstudents (...nums){
-    let total = nums.reduce((acc,n)=>acc+n,0)
-    console.log(total)
+//Challenge 5
+const people = [
+  { name: "Rohan", age: 22 },
+  { name: "Alice", age: 25 },
+  { name: "Bob", age: 20 }
+];
+function avgage(...peps){
+    let Avgagebe = peps.reduce((acc,person)=>acc+person.age,0)
+    let Real_avg = Avgagebe/peps.length
+    console.log(Real_avg)
 }
-let Multstudents = (...num) =>{
- let total1 = num.reduce((acc,n1)=>acc*n1,1)
-console.log(total1)
-}
-Sumstudents(...numbers)
-Multstudents(...numbers)
+avgage(...people)
